abdc48ed465c378750014d8f3784d8c3c8df4750
Konrad Marzec <konrad.marzec@swmansion.com>
2017-03-29 20:58:00 +0200

    fix problem with scele

---
+++ Assets/Ball/Scripts/Gravity.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gravity : MonoBehaviour {
+	public Rigidbody rb;
+	void Start() {
+		rb = GetComponent<Rigidbody>();
+		rb.velocity = new Vector3(0, -50000, 0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
