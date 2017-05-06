1154354876a4333132aac696b201775219975d09
Pawel Wolak <pawelwolakk@gmail.com>
2017-05-06 20:19:28 +0200

    Added team names to HUD

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -4,9 +4,14 @@ using UnityEngine.UI;
 public class Controller : MonoBehaviour {
     private GameController gameController;
     public Text score;
+    public Text nameA;
+    public Text nameB;
 
 	void Start () {
         gameController = GameController.instance;
         gameController.SetScoreUI(score);
+
+        nameA.text = gameController.nameA;
+        nameB.text = gameController.nameB;
     }
 }
